e58a386e8ebba6403aa9b9b4c829df12eba7c94e
zeze1004 <thwjd2717@naver.com>
2020-09-15 17:06:13 +0900

    흑흑 강의 듣고 1717 시작~~

---
+++ zeze1004/1717.cpp
@@ -0,0 +1,16 @@
+// 참고 https://penglog.tistory.com/26
+
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+ 
+int main() {
+    // 아직도 정확히 먼지 모르지만 일단 쓰라하니 쓰자 대충 출력문 시간 적게 드는거
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+   
+    return 0;
+}
