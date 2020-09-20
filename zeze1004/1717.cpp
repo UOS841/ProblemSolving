fae664fed7fbe12e2b183b9d8d121947c71e38d2
zeze1004 <thwjd2717@naver.com>
2020-09-20 23:32:53 +0900

    기초문제 언제 풀어,,^ㅠㅠ^

--- zeze1004/1717.cpp
+++ zeze1004/1717.cpp
@@ -1,16 +1,47 @@
-// 참고 https://penglog.tistory.com/26
-
 #include <iostream>
 #include <vector>
 using namespace std;
  
+vector<int>root;
+ 
+int find(int node) {
+    if (root[node] == node)
+        return node;
+    else
+        return root[node] = find(root[node]);
+}
+ 
+void uni(int a, int b) {
+    int pa = find(a);
+    int pb = find(b);
+    root[pb] = pa;
+}
  
 int main() {
-    // 아직도 정확히 먼지 모르지만 일단 쓰라하니 쓰자 대충 출력문 시간 적게 드는거
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    // 건호 교수님이 말한 빨리 출력하기
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     
-   
+    int num, calc;
+    cin >> num >> calc;
+ 
+    for (int i = 0; i <= num; i++)
+        root.push_back(i);
+ 
+    int cmd, a, b;
+    for (int i = 0; i < calc; i++) {
+        cin >> cmd >> a >> b;
+        if (cmd == 0) {
+            uni(a, b);
+        }
+        else {
+            if (find(a) == find(b))
+                cout << "YES\n";
+            else
+                cout << "NO\n";
+        }
+    }
+    
     return 0;
 }
