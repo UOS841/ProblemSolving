26ad84dadec5afa32125f9e72511fb14f8a17935
gyuZzang <tndud0606@gmail.com>
2021-03-19 21:40:58 +0900

    Update 01052(물병).cpp

--- GyuZzang/01052(물병).cpp
+++ GyuZzang/01052(물병).cpp
@@ -9,7 +9,7 @@ using namespace std;
 int count_left_bottle(string str) {
 	int cnt = 0;
 	for (int i = 0; i < str.size(); i++) {
-		if(str[i] == '1') cnt++;
+		if (str[i] == '1') cnt++;
 	}
 	//cout << cnt;
 	return cnt;
@@ -17,25 +17,24 @@ int count_left_bottle(string str) {
 
 int count_additional_bottle(string str, int left) {
 	int sum = 0;
-	for (int i = str.size()-1; i > 0; i--) {
+	for (int i = str.size() - 1; i >= 0; i--) {
 		int sti = str[i] == '1' ? 1 : 0;
 		sum += sti * pow(2, str.size() - i - 1);
 		if (str[i] == '0') left--;
 		if (!left) return sum;
 	}
-	return -2;
 }
 
 int main() {
 	int num_of_bottle, bottle_limit;
 	cin >> num_of_bottle >> bottle_limit;
 
-	bitset<24> bs(num_of_bottle);
+	bitset<26> bs(num_of_bottle);
 	string bs_string = bs.to_string();
 
 	//cout << bs;
 	int reducing = count_left_bottle(bs_string) - bottle_limit; //# of bottle that have to reduce
-	
+
 	//cout << reducing;
 
 	if (reducing <= 0) {
@@ -45,15 +44,11 @@ int main() {
 
 	int res = count_additional_bottle(bs.flip().to_string(), reducing + 1);
 
-	if (res >= 999) {
-		cout << 0;
-		return 0;
-	}
 
 	cout << res + 1;
-	
+
 	return 0;
-	
+
 	//9->2
 	//0	1	2	3
 	//9
