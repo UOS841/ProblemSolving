abae9c3bf5a2959174e47f2657b57077c01ddad6
handal <handal__@naver.com>
2021-07-25 23:03:54 +0900

    wow

--- handal95/p12904(A와 B).py
+++ handal95/p12904(A와 B).py
@@ -20,35 +20,21 @@ def inputs():
     """
     S = stdin.readline().rstrip()
     T = stdin.readline().rstrip()
-
-    return S, T
+    
+    return list(S), list(T)
 
 
 def solve():
     S, T = inputs()
-
-    count_a = T.count("A")
-    count_b = T.count("B")
-    print(count_a, count_b)
-
-    q = Queue()
-    q.put(S)
-
-    while not q.empty():
-        cur = q.get()
-
-        if len(cur) == len(T):
-            if cur == T:
-                print(1)
-                return
+    
+    while len(S) != len(T):
+        if T[-1] == "A":
+            T.pop()
         else:
-            if cur.count("A") < count_a:
-                q.put(f"{cur}A")
-
-            if cur.count("B") < count_b:
-                q.put(f"{cur[::-1]}B")
-
-    print(0)
+            T.pop()
+            T = T[::-1]
+            
+    print(int(S == T))
 
 
 run()
