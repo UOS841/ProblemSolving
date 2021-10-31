b7da687b242d851d53c1a75fe16501a1dc3bcf17
GyuZzang <tndud0606@gmail.com>
2021-10-31 20:43:21 +0900

    solved 09372

---
+++ GyuZzang/백준/09372.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while(T--){
+        int n_country, n_plane;
+
+        cin >> n_country >> n_plane;
+
+        while(n_plane--){
+            int country_a, country_b;
+            cin >> country_a >> country_b;
+        }
+
+        cout << n_country - 1 ;
+    }
+}
